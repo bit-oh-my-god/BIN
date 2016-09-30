621c861c58c5f3013da71eb7addc55bb927499e9
Wang Frank <wangzhihongji@sina.cn>
2016-09-30 15:08:59 +0800

    first commit

---
+++ succesive_shortest_flow/pre_defintion.h
@@ -0,0 +1,38 @@
+// pre-defined data struct
+// 
+
+#include <array>
+#include <cstddef>
+
+using std::size_t;
+
+typedef size_t vertex_t;
+typedef int value_t;
+
+typedef struct Edge {
+    value_t u;    // capacity
+    value_t b;     //cost
+    vertex_t source_num;
+    vertex_t sink_num; 
+
+} ffsda;
+
+
+typedef struct Vector {
+    vertex_t vertex_num;
+    value_t returnSupplyDemand() {
+        return v;
+    };
+    private :
+
+    value_t v;      // supply and demand 
+
+} ggasxg;
+
+typedef struct Graph {
+    const int vertex_count;
+    Graph (int ver_) 
+        : vertex_count(ver_) {
+            // 
+        }
+} sadhzyq;
---
+++ succesive_shortest_flow/readMe.md
@@ -0,0 +1,6 @@
+### This is a implement for this [paper](https://www.researchgate.net/publication/266993235_Interactive_tool_for_the_successive_shortest_paths_algorithm_in_solving_the_minimum_cost_flow_problem)
+
+##### Reference 
+- [Dijkstra](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
+- [Bellam-Ford](https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm)
+- [Mini-cost-flow](https://en.wikipedia.org/wiki/Minimum-cost_flow_problem)
